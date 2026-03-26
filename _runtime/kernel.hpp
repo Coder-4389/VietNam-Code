3834c2aff04d4552e7fea93052764911e4f7941f
Coder-4389 <nguyen2403klsm@gmail.com>
2026-03-26 20:04:46 +0700

    vnc-260326

--- _runtime/kernel.hpp
+++ _runtime/kernel.hpp
@@ -1,18 +1,55 @@
+#ifndef KERNEL_HPP
+#define KERNEL_HPP
+
+// ****************************************
+// --- start of code ---
+// ****************************************
+
+// --- object lib ---
 #include <string>
 #include <vector>
 #include <map>
+#include <optional>
+
+// --- int lib ---
+#include <cstdint>
+
+// --- exec lib ---
+#include <iostream>
 
 using namespace std;
 
-template<typename t1, typename t2 = string>
-t1 print(t1 text, t2 end = "\n") {
-    cout << text << end;
-    return text;
+template <typename t1>
+using opl = optional<t1>;
+
+constexpr auto null = nullopt;
+
+class ios {
+	public:
+	inline opl<bool> print(auto text, auto end = "\n") {
+		if (cout << text << end) {return true;}
+		return null;
+	}
+
+	inline opl<string> input(auto text = "") {
+		string in_text;
+		if (!print(text,"")) {return null;} 
+		if (!cin >> in_text) {return null;} 
+		return in_text;
+	}
 }
 
-template<typename t1>
-t1 input(t1 text = "") {
-    string input;
-	cout << text; cin >> input;
-    return input;
+extern "C" {
+	
 }
+
+inline opl<size_t> len(auto object) {
+	size_t cnt = object.len();
+	if (cnt >= 0) {return cnt;}
+	return null
+}
+
+// ****************************************
+// --- end of code ---
+// ****************************************
+#endif
