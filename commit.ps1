21825df4748eb85ad4339788445e4e3ed2e9b3a9
Coder-4389 <nguyen2403klsm@gmail.com>
2026-03-31 18:42:36 +0700

    vnc-310326

--- commit.ps1
+++ commit.ps1
@@ -5,20 +5,24 @@ $msg = "vnc-$date"
 
 Write-Host "--- Cleaning git's status ---" -ForegroundColor Cyan
 git rebase --abort 2>$null
-git merge --abort 2>$null
+git merge --abort 2>/dev/null
 
 Write-Host "--- Check file change ---" -ForegroundColor Cyan
 git add . 2>$null
-
 git commit -m "$msg" --allow-empty 2>&1
 
+Write-Host "--- Syncing with GitHub (Pull) ---" -ForegroundColor Cyan
+git pull origin main --rebase 2>&1
+
 Write-Host "--- Push the project to GitHub ---" -ForegroundColor Cyan
-git push origin main --force 2>&1
+git push origin main 2>&1
 
 if ($LASTEXITCODE -eq 0) {
     Write-Host "`n==========================================" -ForegroundColor Green
     Write-Host "      PUSH PROJECT HAS COMPLETED!         " -ForegroundColor Green
     Write-Host "==========================================`n" -ForegroundColor Green
 } else {
-    Write-Host "`n[!] REAL ERROR DETECTED. CHECK CONNECTION." -ForegroundColor Red
-} $ErrorActionPreference = "Continue"
+    Write-Host "`n[!] ERROR: MAYBE CONFLICT OR NETWORK ISSUE." -ForegroundColor Red
+}
+
+$ErrorActionPreference = "Continue"
